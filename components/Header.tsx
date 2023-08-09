da9d581169571c4ce99e9bd96c4a0a9e763c9124
Yaroslav <yarasity24@gmail.com>
2023-08-09 17:51:19 +0300

    blog search update

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,18 @@
 import Link from "next/link";
 import React from "react";
 
-type Props = {};
+import Navigation from "./Navigation";
 
-const Header = (props: Props) => {
+const navItems = [
+  { label: "Home", href: "/" },
+  { label: "Blog", href: "/blog" },
+  { label: "About", href: "/about" },
+];
+
+const Header = () => {
   return (
     <header>
-      <Link href="/">Home</Link>
-      <Link href="/blog">Blog</Link>
-      <Link href="/about">About</Link>
+      <Navigation navLinks={navItems} />
     </header>
   );
 };
